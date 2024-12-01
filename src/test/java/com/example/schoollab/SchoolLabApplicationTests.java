b6c0982a20ba503273a4a98ee456d97d39345f8e
mszablewski <mszablewski2004@gmail.com>
2024-12-01 23:11:31 +0100

    init

---
+++ src/test/java/com/example/schoollab/SchoolLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schoollab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
